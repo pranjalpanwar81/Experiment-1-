971e7324886332e860ddbef68a8e0931678dd34d
pranjalpanwar81 <138666385+pranjalpanwar81@users.noreply.github.com>
2024-07-30 23:25:13 +0530

    Create Pranjal_B2_164_Exp1A_CDS.cpp

---
+++ Pranjal_B2_164_Exp1A_CDS.cpp
@@ -0,0 +1,11 @@
+// NAME - Pranjal Panwar 
+// PRN - 23070123164 
+// EXPERIMENT - 1(A) 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"HELLO WORLD!";     //Output - HELLO WORLD!
+    return 0;
+}
